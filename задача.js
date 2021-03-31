42b71b5049289889490d59373351140500f8fb46
NastyaBubukina <81142561+NastyaBubukina@users.noreply.github.com>
2021-03-31 20:37:25 +0700

    Add files via upload

---
+++ задача.js
@@ -0,0 +1,18 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+movies: {
+"logan": "Я просмотрела 1000 фильмов"
+}
+count: numberOfFilms;
+movies: {};
+actors: {};
+genres: [];
+privat: false;
+const numberOfFilms = +prompt("Один из последних просмотренных фильмов?");
+movies: {
+'logan': 'Сумерки'
+const numberOfFilms = +prompt("На сколько оцените его?");
+movies: {
+'logan': '90 баллов'
+while (personalMovieDB , private != false || (c = 0 || c > 50)){
+console.log(personalMovieDB)
+}
